f72f21debb77b8664f7bc1e4fd2e2cda3f96643f
Huy Nguyen <huynguyen560@gmail.com>
2024-08-18 18:01:32 -0700

    feat: add operate function
    Authorized-by: Huy Nguyen (huynguyen560@gmail.com)

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+const firstNumber = "";
+const secondNumber = "";
+const operator = "";
+
+function operate(firstNumber, secondNumber, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return substract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
+}
